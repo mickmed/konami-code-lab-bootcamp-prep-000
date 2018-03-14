3fc3a6e727235271b2850f8a04f518c86a4229b2
Michael Roth <mick2090@gmail.com>
2018-03-14 18:21:53 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  const docBody = document.body.getElementByTag('input');
+  const docBody = document.body.getElementByTagName;
   docBody.addEventListener('click', function(event) {
   alert('I was clicked!')})
   const alphabet = [65, 66, 67];

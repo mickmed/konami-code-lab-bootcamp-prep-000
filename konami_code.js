d80203c45f396c01f0e5e4b3af54605f9e6345b3
Michael Roth <mick2090@gmail.com>
2018-03-14 18:01:06 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -10,12 +10,12 @@ function init() {
   function onKeyDownHandler(e) {
     const key = parseInt(e.detail || e.which);
     console.log(key);
+   
     if (key === alphabet[index]) {
       index++;
    
       if (index === alphabet.length) {
         alert("Hurray!");
-   
         index = 0;
       }
     } else {

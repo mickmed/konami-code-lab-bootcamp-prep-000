328660df05dfe8f754df58832fc87b56377899d1
Michael Roth <mick2090@gmail.com>
2018-03-14 17:51:14 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -9,30 +9,22 @@ function init() {
   const alphabet = [65, 66, 67];
  
 // Keep track of index outside of the event handler.
-let index = 0;
+  let index = 0;
  
-// This is the function that would be invoked by the event listener.
-function onKeyDownHandler(e) {
-  const key = parseInt(e.detail || e.which);
- 
-  if (key === alphabet[index]) {
-    index++;
- 
-    if (index === alphabet.length) {
-      alert("Hurray!");
- 
+
+  function onKeyDownHandler(e) {
+    const key = parseInt(e.detail || e.which);
+   
+    if (key === alphabet[index]) {
+      index++;
+   
+      if (index === alphabet.length) {
+        alert("Hurray!");
+   
+        index = 0;
+      }
+    } else {
       index = 0;
     }
-  } else {
-    index = 0;
   }
-}
-
-  
-  
-  
-  
-  
-  
-  
 }

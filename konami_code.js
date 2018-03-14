81797aa5ee938090161d9128553560a365501e2d
Michael Roth <mick2090@gmail.com>
2018-03-14 18:06:45 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,9 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   const docBody = document.body;
-  docBody.addEventListener('keydown', onKeyDownHandler) 
+  docBody.addEventListener('keydown', function(e) {
+  console.log(e.which)
+})
   const alphabet = [65, 66, 67];
   let index = 0;
   function onKeyDownHandler(e) {

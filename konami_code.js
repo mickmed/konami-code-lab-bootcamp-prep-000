ab4672ea2c469b062362ccc0eca7c82bb8fe01f8
Michael Roth <mick2090@gmail.com>
2018-03-14 18:46:59 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,16 +3,15 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   const docBody = document.querySelector('input')
   docBody.addEventListener('keydown', onKeyDownHandler)
-  const kcode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
   console.log(code)
   let index = 0;
   function onKeyDownHandler(e) {
     const key = parseInt(e.detail || e.which);
     console.log(key);
-    if (key === kcode[index]) {
+    if (key === code[index]) {
       index++;
    
-      if (index === kcode.length) {
+      if (index === code.length) {
         alert("Hurray!");
         index = 0;
       }

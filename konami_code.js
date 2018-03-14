074b7d21904adeccea6e8209137cec43bb161836
Michael Roth <mick2090@gmail.com>
2018-03-14 17:43:10 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,16 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   const docBody = document.body;
   
-  docBody.addEventListener('click', function(event) {
-  alert('I was clicked!')
-})
+  docBody.addEventListener('keydown', function(e) {
+    alert('Hurray!')
+  })
+  
+  
+  
+  
+  
+  
+  
+  
+  
 }
